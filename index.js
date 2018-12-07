bc02dfc649bd21ce6e312f85a54a2d8b2d5858b0
kiranbhalerao123 <bhaleraok65@gmail.com>
2018-12-07 15:39:22 +0530

    removed silly console.log

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 require("./utils/decodeCase64");
 require("./utils/encode");
 
-
-console.log("Your String.".encodeToLowerCase());
-console.log("Your String.".encodeToUpperCase());
-
-console.log("-w-w91ci-b-td-h-jpbmcu".decodeCase64());
-console.log("WW91-C-IBT-DHJ-P-B-M-C-U".decodeCase64());
